076f7e2d0a31af0d4ebe54b255fc94ca795430a6
Fengshuang Tan <ftan@redhat.com>
2026-01-30 14:48:48 +0800

    update

--- ci-operator/step-registry/acm/tests/clc-create/try2/acm-tests-clc-create-try2-commands.sh
+++ ci-operator/step-registry/acm/tests/clc-create/try2/acm-tests-clc-create-try2-commands.sh
@@ -23,11 +23,9 @@ if [[ -f "${awsCredFile}" ]]; then
     set +x
     awsAccKeyID="$(sed -nE 's/^\s*aws_access_key_id\s*=\s*//p;T;q' "${awsCredFile}")"
     awsAccKeyToken="$(sed -nE 's/^\s*aws_secret_access_key\s*=\s*//p;T;q' "${awsCredFile}")"
-    set -x
 
     [ -n "${awsAccKeyID}" ] && [ -n "${awsAccKeyToken}" ]
 
-    set +x
     yq -o json eval . "${optionFile}" |
     jq -c \
           --arg awsAccKeyID "${awsAccKeyID}" \
